d6d6b3da13369a7d8aab41b4f5ebf2df39d628da
ZivDero <kirill.andriiashin@gmail.com>
2026-08-29 03:41:16 +0300

    Start a session node with nothing asked for

--- code/netdlg2.cpp
+++ code/netdlg2.cpp
@@ -852,7 +852,6 @@ bool Net2Remote_Connect(void)
 				// Add myself to the list, and to the Players vector.
 				//------------------------------------------------------------------------
 				NodeNameType * who = new NodeNameType;
-				memset(who, 0, sizeof(*who));
 				strcpy(who->Name, Session.Handle);
 				strcpy(who->Player.Serial, SerialNumber);
 				who->Player.House = Session.House;
@@ -2474,7 +2473,6 @@ static void Get_Join_Responses(void)
 				// Create & add a node to the Vector
 				//..................................................................
 				who = new NodeNameType;
-				memset(who, 0, sizeof(*who));
 				strcpy(who->Name, Session.GPacket.Name);
 				strcpy(who->Player.Serial, Session.GPacket.Serial);
 				who->Address = Session.GAddress;
@@ -2531,7 +2529,6 @@ static void Get_Join_Responses(void)
 				Clear_Vector(&Session.Players);
 
 				who = new NodeNameType;
-				memset(who, 0, sizeof(*who));
 				strcpy(who->Name, Session.Handle);
 				who->Player.House = Session.House;
 				who->Player.Color = Session.ColorIdx;
@@ -3105,7 +3102,6 @@ static void Get_Join_Responses(void)
 				// Add node to the Vector list
 				//..................................................................
 				who = new NodeNameType;
-				memset(who, 0, sizeof(*who));
 				strcpy(who->Name, Session.GPacket.Name);
 				who->Address = Session.GAddress;
 				who->Player.House = Session.GPacket.PlayerInfo.House;
--- code/session.h
+++ code/session.h
@@ -44,6 +44,8 @@
 #include "version.h"
 #include "win.h"
 
+#include <cstring>
+
 #include "dialog.hh"
 #include "diff.hh"
 
@@ -217,6 +219,9 @@ struct NodeNameType {
 			int ProcessTime;			// Length of time to process players main loop
 			int Status;					//
 			int SquadID;				//
+			int SpawnChoice;			// starting waypoint asked for; -1 = the engine picks
+			int Handicap;				// difficulty asked for; -1 = the session default
+			unsigned AlliesMask;		// seats allied with, one bit per seat index
 		} Player;
 		struct {
 			unsigned int LastTime;		// last time we heard from this guy
@@ -224,6 +229,17 @@ struct NodeNameType {
 			int Color;					// chat player's color
 		} Chat;
 	};
+
+	/*
+	 * A node starts with nothing asked for, so that a session source which names none of
+	 * these leaves the game its own choice of start position and difficulty.
+	 */
+	NodeNameType(void)
+	{
+		memset(this, 0, sizeof(*this));
+		Player.SpawnChoice = -1;
+		Player.Handicap = -1;
+	}
 };
 
 
