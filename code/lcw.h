2c7bac2bbf972defc96113ee0ea399f8a57bf19e
Jakub Vesely <1251980+tinix0@users.noreply.github.com>
2026-09-03 20:18:33 +0200

    Cleanup lcw.h

--- code/lcw.h
+++ code/lcw.h
@@ -35,10 +35,4 @@
 
 uint32_t LCW_Uncomp(void const * source, void * dest, unsigned long length=0);
 
-#ifdef _MSC_VER
 int LCW_Comp(void const * source, void * dest, int length);
-#else
-extern "C" {
-int __cdecl LCW_Comp(void const * source, void * dest, int length);
-}
-#endif
