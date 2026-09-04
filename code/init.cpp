69a8333563ec174cab16d811d8c8483c3977ac0e
torradmin <torradmin@gmail.com>
2026-09-04 03:07:41 +0300

    * Added support for continuous scrolling using keyboard key binds for "Scroll East/West/North/South" instead of them basically being useless keybinds. Scroll speed with keyboard is similar to edge scrolling and takes into account the "Scroll speed" slider in options.
    (cherry picked from commit 6956eff5aec6044bcd3f93383e182d13f73527ac)
    (cherry picked from commit e5f16564fc17b1ffcdab08e89309dceb51358b86)

--- code/init.cpp
+++ code/init.cpp
@@ -4268,8 +4268,11 @@ class ScrollNCommandClass : public CommandClass
 		}
 
 		virtual void Execute(void) const {
-			int distance = 34;
+			int distance = Map.Get_Keyboard_Scroll_Distance();
 			Map.Scroll_Map(FACING_N, distance, true);
+		}
+		virtual bool Is_Repeatable(void) const {
+			return(true);
 		}
 };
 
@@ -4291,8 +4294,11 @@ class ScrollSCommandClass : public CommandClass
 		}
 
 		virtual void Execute(void) const {
-			int distance = 34;
+			int distance = Map.Get_Keyboard_Scroll_Distance();
 			Map.Scroll_Map(FACING_S, distance, true);
+		}
+		virtual bool Is_Repeatable(void) const {
+			return(true);
 		}
 };
 
@@ -4314,8 +4320,11 @@ class ScrollECommandClass : public CommandClass
 		}
 
 		virtual void Execute(void) const {
-			int distance = 34;
+			int distance = Map.Get_Keyboard_Scroll_Distance();
 			Map.Scroll_Map(FACING_E, distance, true);
+		}
+		virtual bool Is_Repeatable(void) const {
+			return(true);
 		}
 };
 
@@ -4337,8 +4346,11 @@ class ScrollWCommandClass : public CommandClass
 		}
 
 		virtual void Execute(void) const {
-			int distance = 34;
+			int distance = Map.Get_Keyboard_Scroll_Distance();
 			Map.Scroll_Map(FACING_W, distance, true);
+		}
+		virtual bool Is_Repeatable(void) const {
+			return(true);
 		}
 };
 
