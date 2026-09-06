960fa9eab532a5df84a82c911e2997cb9800566f
Gunnar Beutner <gunnar@beutner.de>
2026-09-06 17:04:05 +0200

    Assert the on-disk structure sizes

--- code/isotype.h
+++ code/isotype.h
@@ -104,6 +104,7 @@ struct IsoTileRecord
 	RGBStruct LowColor;
 	RGBStruct HighColor;
 };
+static_assert(sizeof(IsoTileRecord) == 52, "a TMP tile record is 52 bytes on disk");
 #pragma pack()
 
 #pragma pack(4)
@@ -189,6 +190,7 @@ class IsoTileSet
 		IsoTileSet(IsoTileSet const & rvalue);
 		IsoTileSet const & operator = (IsoTileSet const & rvalue);
 };
+static_assert(sizeof(IsoTileSet) == 20, "the TMP header is 16 bytes on disk, followed by the four-byte tile offsets");
 #pragma pack()
 
 
