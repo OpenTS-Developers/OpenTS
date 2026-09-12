edd9ab643817e195d4e5ff4a5c5d837117e12c4d
ZivDero <kirill.andriiashin@gmail.com>
2026-09-12 09:30:42 +0300

    Attribute the find-file declarations in file.h to EA

--- code/file.h
+++ code/file.h
@@ -2,13 +2,15 @@
  *                                O P E N  T S
  *******************************************************************************
  * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright 2020 Electronic Arts Inc.
  * Copyright 2020-2024 Vanilla Conquer contributors
  * Copyright 2026 OpenTS contributors
  *
- * Contains material derived from Vanilla Conquer
- * (https://github.com/TheAssemblyArmada/Vanilla-Conquer).
+ * Contains material derived from Electronic Arts source code and from Vanilla
+ * Conquer (https://github.com/TheAssemblyArmada/Vanilla-Conquer).
  * Modified by OpenTS contributors, 2026.
- * See LICENSE.md for applicable additional terms and warranty disclaimers.
+ * EA's GPLv3 Section 7 additional terms and supplemental warranty
+ * disclaimers apply; see LICENSE.md.
  ******************************************************************************/
 
 #pragma once
